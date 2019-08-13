03159138b17561e2d59b14ccdda9eb216d2d0ef1
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-13 16:39:16 -0400

    adds AnimalListCell cocoa file

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/AnimalListCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimalListCell.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Radharani Ribas-Valongo on 8/13/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class AnimalListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
